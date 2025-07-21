d04f0d837415461516bd1d76331ee0ebb80458be
Anagh Patel <anagh0106@gmail.com>
2025-07-21 17:40:48 +0530

    Password

--- util/SendMailFaculty.js
+++ util/SendMailFaculty.js
@@ -1,17 +1,21 @@
 const mailer = require("nodemailer");
 require("dotenv").config({ path: "../.env" })
-
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 const sendMail = async (to, subject, text) => {
     const transport = mailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
         secure: true,
+        // auth: {
+        //     user: process.env.EMAIL_USER,
+        //     pass: process.env.EMAIL_PASS
+        // }
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: "anagh0106@gmail.com",       // Exact same email
+            pass: "gfzhwajbdcdqpttd" // Copy-paste from Google App Passwords
         }
     });
-    console.log(process.env.EMAIL_USER);
 
     const mailOptions = {
         from: `"Anagh" <${process.env.EMAIL_USER}>`, // Ensure sender email matches the authenticated account
